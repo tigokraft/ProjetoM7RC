e2763ee6cb90aca1037092f87c15aa5431da0cef
Tiago Cunha De Oliveira <2223234@alunos.escoladigital.com>
2025-12-12 16:16:41 +0000

    added login to auth

---
+++ src/app/api/auth/login/route.ts
@@ -0,0 +1,68 @@
+import { NextResponse, NextRequest } from "next/server";
+import * as z from "zod";
+import { PrismaClient } from "@prisma/client";
+import { comparePassword } from "@/lib/Hasher";
+import { signToken } from "@/lib/Jwt";
+import { serialize } from "cookie";
+
+const prisma = new PrismaClient();
+
+const userObj = z.object({
+  email: z.string().email(),
+  password: z.string().min(8),
+});
+
+export async function POST(req: NextRequest) {
+  let parsedBody;
+
+  try {
+    parsedBody = userObj.safeParse(await req.json());
+    if (!parsedBody.success) {
+      return NextResponse.json(
+        { error: "Invalid request data", details: parsedBody.error },
+        { status: 400 }
+      );
+    }
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Invalid JSON format", details: error },
+      { status: 400 }
+    );
+  }
+
+  try {
+    const { email, password } = parsedBody.data;
+    const user = await prisma.user.findUnique({
+      where: { email: email },
+    });
+    if (!user) {
+      return NextResponse.json({ error: "Email not found" }, { status: 401 });
+    }
+
+    if (!comparePassword(password, user.password)) {
+      return NextResponse.json({ error: "Invalid password" }, { status: 401 });
+    }
+
+    const token = signToken({ userId: user.id, email: user.email });
+
+    const response = NextResponse.json({
+      message: "Login successful",
+      token,
+    });
+
+    response.cookies.set("auth_token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict",
+      path: "/",
+      maxAge: 7 * 24 * 60 * 60,
+    });
+
+    return response;
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Internal server error", details: error },
+      { status: 500 }
+    );
+  }
+}
